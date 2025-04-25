bdbb73a3473f7a3d924643d3f0b49fb165b0314d
Thais Tanaka <thais.tanaka@hotmail.com>
2025-04-25 17:04:22 -0300

    finalizacao

--- automacao.py
+++ automacao.py
@@ -9,14 +9,15 @@ import pyautogui as pg
 #hotkey > atalho (ctrl + c, ctrl + v, etc)
 #moveTo > move o mouse para a posição (x, y)
 
-pg.PAUSE = 0.5 #pausa de 0.5 segundo entre os comandos
+pg.PAUSE = 0.3 #pausa de 0.3 segundo entre os comandos
 
 #entrar no sistema
 pg.press('win')
 pg.write('chrome')
 pg.press('enter')
 
-pg.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+site = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
+pg.write(site)
 pg.press('enter')
 time.sleep(3) #espera 3 segundos para carregar a página
 
@@ -30,5 +31,37 @@ pg.press('enter')
 time.sleep(3) 
 
 #importar dados
-tabela = pd.read_csv('produtos.csv')
-pg.click(x=-1156, y=424) 
+tabela = pd.read_csv('produtos.csv') #se o arquivo estiver na mesma pasta do script, não precisa do caminho completo
+   
+#cadastrar produtos
+for l in tabela.index: 
+    pg.click(x=-1156, y=424)
+    codigo = tabela.loc[l, "codigo"] 
+    pg.write(str(codigo)) 
+    pg.press('tab')
+    marca = tabela.loc[l, "marca"] 
+    pg.write(str(marca))
+    pg.press('tab')
+    tipo = tabela.loc[l, "tipo"] 
+    pg.write(str(tipo)) 
+    pg.press('tab')
+    categoria = tabela.loc[l, "categoria"] 
+    pg.write(str(categoria))
+    pg.press('tab')
+    preco_unitario = tabela.loc[l, "preco_unitario"] 
+    pg.write(str(preco_unitario))
+    pg.press('tab')
+    custo = tabela.loc[l, "custo"] 
+    pg.write(str(custo))
+    pg.press('tab')
+    obs = tabela.loc[l, "obs"] 
+    # quando o valor for vazio p pandas chama de nan
+    if pd.isna(obs):
+        pg.write('')
+    else:
+        pg.write(str(obs))
+    pg.press('tab')
+    pg.press('enter')
+    pg.scroll(10000)
+    # scroll positivo = sobe 10000 pixels
+    # scroll negativo = desce 10000 pixels
